d4f4e4601908d072037c3e5d5a9c6f24667239b6
TruongDayNe <153939145+TruongDayNe@users.noreply.github.com>
2024-03-17 15:28:15 +0700

    Create library.hpp

---
+++ fileIO/library.hpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+float console() {
+    float choose;
+    do {
+        cout << << endl << "============== WORD  -  GAME ==============" << endl;
+        cout << "|   1. Input matrix manually                 |" << endl;
+        cout << "|   2. Read matrix from file                 |" << endl;
+        cout << "|   3. Matrices addition                     |" << endl;
+        cout << "|   4. Matrices multiplication               |" << endl;
+        cout << "|   5. Permute 2 collumns of a matrix        |" << endl;
+        cout << "|   6. Close program                         |" << endl;
+        cout << "===========================================" << endl;
+        cout << "Choose your option: ";
+        cin >> choose;
+
+        if (!((choose == static_cast<int>(choose)) && (choose <= 7 && choose >= 1))) 
+        {
+            clrscr;
+            cout << "\nFailed! Please choose again!" << endl;
+        } 
+        else {
+            break;
+        }
+    } while (!((choose == static_cast<int>(choose)) && (choose <= 7 && choose >= 1)));
+    return choose;
+}
+
+int main() {
+    float userChoice = console();
+    // Handle user choice based on the returned value
+    // ...
+    return 0;
+}
