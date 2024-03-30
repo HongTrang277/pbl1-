27952af45856a598ca8ad233daf59e92a93bcc5b
TruongDayne <lostpeanut123@gmail.com>
2024-03-30 09:52:49 +0700

    he

--- fileIO/consoleLib.hpp
+++ fileIO/consoleLib.hpp
@@ -172,10 +172,10 @@ void Menu()
                 cout << "Which collumns you want to permute:" << "(Enter number from 0 - "<< size-1 << ")" << endl;
                 cout << "Col 1: "; cin >> col1;
                 if (col1 < 0 || col1 > size-1)
-                    cout << "Invaliid collumn"; goto INVALID;
+                    cout << "Invaliid collumn" << endl; goto INVALID;
                 cout << "Col 2: "; cin >> col2;
                 if (col2 < 0 || col2 > size-1)
-                    cout << "Invaliid collumn"; goto INVALID;
+                    cout << "Invaliid collumn" << endl; goto INVALID;
                 PermuteCol(result, size1, col1, col2, preChoice);
                 cout << " Collumns permuted" << endl;
                 cin.ignore(1000 , '\n');
